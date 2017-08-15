0e9e1de2b63603392cf170ee55751dce30d10f9a
jon <mail@jonclawson.com>
2017-08-15 14:03:06 -0700

    fixed dropdowns for mulitple

--- client/app/_directives/dropdown.directive.ts
+++ client/app/_directives/dropdown.directive.ts
@@ -3,13 +3,18 @@ import { Directive, ElementRef, Input } from '@angular/core';
 @Directive({ selector: '[dropdownDirective]' })
 export class dropdownDirective {
     constructor(el: ElementRef) {
-       el.nativeElement.onclick = function(e){
-            if(el.nativeElement.className.indexOf('open')> -1){
-               el.nativeElement.className = el.nativeElement.className.replace('open', '');
-            }
-            else{
-                el.nativeElement.className+= ' open';
-            }
-       }
+    document.addEventListener('click', function(event) {
+      var isClickInside = el.nativeElement.contains(event.target);
+      if (!isClickInside) {
+        el.nativeElement.className = el.nativeElement.className.replace('open', '');
+      }else{
+          if(el.nativeElement.className.indexOf('open')> -1){
+             el.nativeElement.className = el.nativeElement.className.replace('open', '');
+          }
+          else{
+              el.nativeElement.className+= ' open';
+          }
+      }
+    });
     }
 }
